19faab48d082015c4c64b57010931e77513480cc
dhandyjoe <dhanjoenkp@gmail.com>
2022-07-28 10:10:02 +0700

    add put update user

--- src/test/java/response/ReqresResponse.java
+++ src/test/java/response/ReqresResponse.java
@@ -7,5 +7,6 @@ public class ReqresResponse {
     public static final String NAME = "name";
     public static final String EMAIL = "email";
     public static final String PHONE = "phone";
+    public static final String ID = "id";
 
 }
